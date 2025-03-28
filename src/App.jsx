cf2b67d3d47161d7ae5db3b98b1fddcc9a643dad
Malveira16 <joa.malveira@gmail.com>
2025-03-28 13:00:16 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Header } from "./components/Header"
+import {Post} from "./post"
+
+import './styles.css'
+export function App() {
+  return(
+     <div>
+      <Header/>
+      <Post 
+        author = "João Vitor Malveira" 
+        content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore vero earum quibusdam hic dolorum id, mollitia dolore, a recusandae sunt, eos nisi consequatur dolores nihil officiis tempora vel odio necessitatibus." >
+
+      </Post>
+     </div>
+  )
+}
